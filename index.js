8ec0598d6740ff968340c8b9e05a0c63d8ce5e6b
nasrudinfahmi <nasrudin.fhm@gmail.com>
2024-08-02 22:04:11 +0700

    try vercel

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import helmet from "helmet";
-import serverless from "serverless-http";
 import "dotenv/config";
 
 const app = express();
@@ -14,18 +13,10 @@ app.get("/", (_req, res) => {
   return res.status(200).send("Hello World!");
 });
 
-router.get("/coba", (_req, res) => {
-  return res.json({
-    success: true,
-    status: 200,
-    message: "Berhasil",
-  });
+app.get("/home", (_req, res) => {
+  return res.status(200).send("Welcome Home");
 });
-
-app.use("/.netlify/functions/api", router);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}.`);
 });
-
-export const handler = serverless(app);
